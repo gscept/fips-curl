d299791d136dc8f6d00ed1f75ee9408f50e4ea64
Johannes <kaffeewolf@users.noreply.github.com>
2020-12-26 16:48:35 +0100

    CI test

--- fips-include.cmake
+++ fips-include.cmake
@@ -1,3 +1,3 @@
 if(FIPS_WINDOWS)
-    configure_file(${CMAKE_CURRENT_LIST_DIR}/bin/win64/libcurl-x64.dll ${FIPS_DEPLOY_DIR}/${CMAKE_PROJECT_NAME}/${FIPS_CONFIG} COPYONLY)
+    #configure_file(${CMAKE_CURRENT_LIST_DIR}/bin/win64/libcurl-x64.dll ${FIPS_DEPLOY_DIR}/${CMAKE_PROJECT_NAME}/${FIPS_CONFIG} COPYONLY)
 endif()    
